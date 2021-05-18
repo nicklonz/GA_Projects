f6b4419b979dcdc9681591dd8fc0340cecc1421d
nicklonz <nicklonz@gmail.com>
2021-05-17 21:58:31 -0400

    completed part 1

--- react-film/src/App.js
+++ react-film/src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FilmListing from './FilmListing';
+import FilmDetails from './FilmDetails';
+import TMDB from './TMDB';
 
 class App extends Component {
   render() {
     return (
-      <div className="film-library">
-        <div className="film-list">
-          <h1 className="section-title">FILMS</h1>
-        </div>
-
-        <div className="film-details">
-          <h1 className="section-title">DETAILS</h1>
+      <div>
+        <div className="film-library">
+          <FilmListing films={TMDB.films} />
+          <FilmDetails films={TMDB.films} />
         </div>
       </div>
     );
