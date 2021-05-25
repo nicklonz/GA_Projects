752fe66ab48e741156cd867175468bcb3adfb024
nicklonz <nicklonz@gmail.com>
2021-05-24 20:17:05 -0400

    completed project

--- hardware_store/src/App.js
+++ hardware_store/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+  import logo from './logo.svg';
+  import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  import React, { Component } from 'react';
+  import HomePage from './HomePage';
+
+  class App extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+        saleItem: "Screw Driver!!!",
+        editable: true,
+        discount: 5      
+      }
+    }
+
+    toggleEditSaleItem = () => {
+      this.setState({
+      editable: !this.state.editable
+    })
+  }
+
+    adjustDiscount = (num) => {
+      this.setState({
+        discount: this.state.discount + num
+      })
+    }
+
+    handleItemChange = (event) => {
+      this.setState({
+        saleItem: event.target.value
+      })
+    }
+
+    render() {
+      return (
+      <div className="App">
+          <h1>Welcome to the Hardware Store</h1>
+          <HomePage 
+            itemOnSale={this.state.saleItem} 
+            editable={this.state.editable}
+            toggleEditSaleItem={this.toggleEditSaleItem}
+            discount={this.state.discount}
+            adjustDiscount={this.adjustDiscount}
+            handleItemChange={this.handleItemChange}
+          />
+      </div>
+    );
+  }
 }
 
 export default App;
