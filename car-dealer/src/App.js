82c943fc4c528ed2af23c75dcbd619e444d3034e
nicklonz <nicklonz@gmail.com>
2021-05-30 16:06:17 -0400

    working project

--- car-dealer/src/App.js
+++ car-dealer/src/App.js
@@ -8,19 +8,46 @@ class App extends Component {
     super(props);
 
     this.state = {
+
+      carSale: "Junk Heap",
+      editable: true,
+      discount: 10,
       username: "Mr. Customer",
-      availableCars: ["Alpha Romeo", "Mercedes", "BMW", "Junk Heap", "Whatever", "Tesla"]
+      availableCars: ["XYZ Auto","Alpha Romeo", "Creep Jeep","Mercedes", "BMW", "Junk Heap", "Whatever", "Tesla"]
     }
   }
 
-  render() {
+  toggleEditCarSpecial = () => {
+    this.setState({
+      editable: !this.state.editable
+    })
+  }
 
+  adjustDiscount = (num) => {
+    this.setState({
+      discount: this.state.discount + num
+    })
+  }
+ 
+  handleItemChange = (event) => {
+    this.setState({
+      carSale: event.target.value
+    })
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>Car Dealer</h1>
         <Inventory
           username={this.state.username}
+          carSale={this.state.carSale}
+          editable={this.state.editable}
+          toggleEditCarSpecial={this.toggleEditCarSpecial}
           availableCars={this.state.availableCars.sort()}
+          discount={this.state.discount}
+          adjustDiscount={this.adjustDiscount}
+          handleItemChange={this.handleItemChange}
         />
       </div>
     );
--- car-dealer/src/Inventory.js
+++ car-dealer/src/Inventory.js
@@ -4,8 +4,22 @@ const Inventory = (props) => {
   return (
     <div>
       <h2>Hello, {props.username}</h2>
-
-      <h3>Vehicles Available from A to Z are: </h3>
+      <h3>Vehicle Markdown Sale now on the<h2>{props.carSale}</h2> for an additional <h2>{props.discount}% off!!!</h2></h3>
+      <button
+        onClick={props.toggleEditCarSpecial}
+      >{props.editable ? "Complete" : "Edit Car Special"}</button>
+      { props.editable && (
+      <div>
+        <input 
+              type="text" 
+              value={props.carSale}
+              onChange={props.handleItemChange} 
+        />
+          <button onClick={() => props.adjustDiscount(1)}>+</button>
+          <button onClick={() => props.adjustDiscount(-1)}>-</button>
+      </div>
+      )}
+      <h3>Vehicles available from A to Z are: </h3>
       
         {props.availableCars.map((car) =>
           <p><li>{car}</li></p>
